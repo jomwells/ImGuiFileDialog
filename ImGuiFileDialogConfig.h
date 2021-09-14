f3e1e1c2cb5ce67ceba18068c5380893574b97e8
Jack Wells <jack.wells@outlook.com>
2021-09-14 15:56:17 +0100

    configuration

--- ImGuiFileDialogConfig.h
+++ ImGuiFileDialogConfig.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "../IconFontCppHeaders/IconsFontAwesome5.h"
+
+#define USE_IMGUI_TABLES
 // uncomment and modify defines under for customize ImGuiFileDialog
 
 //#define MAX_FILE_DIALOG_NAME_BUFFER 1024
@@ -19,22 +22,22 @@
 
 // widget
 // filter combobox width
-//#define FILTER_COMBO_WIDTH 120.0f
+#define FILTER_COMBO_WIDTH 240.0f
 // button widget use for compose path
 //#define IMGUI_PATH_BUTTON ImGui::Button
 // standard button
 //#define IMGUI_BUTTON ImGui::Button
 
 // locales string
-//#define createDirButtonString "+"
-//#define okButtonString " OK"
-//#define cancelButtonString " Cancel"
-//#define resetButtonString "R"
-//#define drivesButtonString "Drives"
-//#define searchString "Search"
-//#define dirEntryString "[DIR] "
-//#define linkEntryString "[LINK] "
-//#define fileEntryString "[FILE] "
+#define createDirButtonString ICON_FA_FOLDER_PLUS
+#define okButtonString (std::string(ICON_FA_CHECK_SQUARE) + " OK").c_str()
+#define cancelButtonString (std::string(ICON_FA_BACKSPACE) +" Cancel").c_str()
+#define resetButtonString ICON_FA_UNDO
+#define drivesButtonString ICON_FA_SERVER
+#define searchString ICON_FA_SEARCH
+#define dirEntryString ICON_FA_FOLDER
+#define linkEntryString ICON_FA_LINK
+#define fileEntryString ICON_FA_FILE
 //#define fileNameString "File Name : "
 //#define dirNameString "Directory Path :"
 //#define buttonResetSearchString "Reset search"
@@ -53,9 +56,9 @@
 //#define DateTimeFormat "%Y/%m/%d %i:%M%p"
 
 // theses icons will appear in table headers
-//#define USE_CUSTOM_SORTING_ICON
-//#define tableHeaderAscendingIcon "A|"
-//#define tableHeaderDescendingIcon "D|"
+#define USE_CUSTOM_SORTING_ICON
+#define tableHeaderAscendingIcon ICON_FA_SORT_ALPHA_UP
+#define tableHeaderDescendingIcon ICON_FA_SORT_ALPHA_DOWN
 //#define tableHeaderFileNameString " File name"
 //#define tableHeaderFileTypeString " Type"
 //#define tableHeaderFileSizeString " Size"
@@ -66,5 +69,5 @@
 //#define IMGUI_TOGGLE_BUTTON ToggleButton
 //#define bookmarksButtonString "Bookmark"
 //#define bookmarksButtonHelpString "Bookmark"
-//#define addBookmarkButtonString "+"
-//#define removeBookmarkButtonString "-"
+//#define addBookmarkButtonString ICON_FA_PLUS_SQUARE
+//#define removeBookmarkButtonString ION_FA_MINUS_SQUARE
